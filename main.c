16be44de4fde367bb58101e29d7663eb7c754cee
t1coz <66951820+t1coz@users.noreply.github.com>
2023-03-29 12:19:10 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,14 +2,18 @@
 #include "words.h"
 #include "functionsForWords.h"
 int main(){
-    struct Node node = {0, NULL, NULL};
-    char *filePath;
-    printf("Write INITIAL text file path: ");
+    char *filePath, *minLenWord, *maxLenWord;
+    struct Unit unit = {0, NULL, NULL};
+    printf("\033[0;31mBeginning of the program.\033[0m\n");
+    printf("Write the file path: ");
     getStr(&filePath);
-    readFile(&node, filePath);
-
-    fileCompression(filePath, &node);
-    fileDecompression("compressedFile.txt");
-    freeList(&node);
+    readFile(&unit, filePath);
+    display(&unit);
+    maxLenWord = findMaxLenWord(&unit);
+    minLenWord = findMinLenWord(&unit);
+    printf("Buffer: %s;\nNew word: %s.\n ", maxLenWord, minLenWord);
+    strangeCompression(filePath, maxLenWord, minLenWord);
+    decompression(filePath);
+    deletingList(&unit);
     return 0;
 }
