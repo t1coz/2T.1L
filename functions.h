39209545cfd4908e7dd7455e42af284c24e77627
t1coz <66951820+t1coz@users.noreply.github.com>
2023-03-29 12:22:00 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -6,20 +6,9 @@
 #ifndef ARCHIVER_FUNCTIONS_H
 #define ARCHIVER_FUNCTIONS_H
 void getStr(char **string);
-int cycleWord(const char *token, char *temp);
-
-void readFile(struct Node *node, const char *fileName);
-
-char *findMaxLenPopularWord(const struct Node *node);
-int getMaxCount(const struct Node *node);
-char *findMinLenRareWord(const struct Node *node);
-int getMinCount(const struct Node *node);
-
-void deleteSymbol(char *temp);
-void checkTransitionToANewLine(char *temp, const char *token);
-void inputSpaces(const char *buffer, char *temp, int i);
-void token(char *buffer, char *temp, char **array, int size);
-char** fillingVocabulary(const char *filePath, struct Node *node, int *size);
-int fileCompression(const char *filePath, struct Node *node);
-int fileDecompression(const char *compressedFilePath);
+void readFile(struct Unit *unit, const char *filePath);
+int getMaxCount(const struct Unit *unit);
+char* findMaxLenWord(const struct Unit *unit);
+char* findMinLenWord(const struct Unit *unit);
+int strangeCompression(const char *filePath, const char *word, const char *newWord);
 #endif //ARCHIVER_FUNCTIONS_H
